9824ba32447aa46265f425d3570a664ec423ffbe
drcdh <danielrhunter@gmail.com>
2018-10-15 17:50:31 -0400

    Remove use of sacred, attempt feature-selection

--- fw_ex_rfc.py
+++ fw_ex_rfc.py
@@ -3,56 +3,129 @@ from __future__ import division, print_function
 import numpy as np
 import pandas as pd
 
+from pprint import pformat, pprint
+
 from hyperopt import hp
-from hyperopt import fmin, tpe, STATUS_OK
-from sacred import Experiment
+from hyperopt import fmin, space_eval, tpe, STATUS_OK, Trials
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import cross_val_score
-#from sklearn.metrics import f1_score
+from sklearn.utils import shuffle as sk_shuffle
 
 import fw_data
 import util
 
 from hyperspaces.rfc import get_space as get_rfc_space
 
-ex = Experiment('facet_wealth_rfc')
+cfg = {
+    'random_seed': 59,
+    'shuffle': True,
+    'onehot': False,
+    'class_weight': 'balanced',
+    'imp_cut': .0,
+    'scoring': 'roc_auc',
+    'scoring_buffer': .05,
+    'cv': 10,
+    'max_evals': 1000,
+    'n_rec': 1,
+}
 
-@ex.config
-def cfg():
-    random_seed = 59
-    n_estimators = 1000
-    class_weight = 'balanced'
-    scoring='roc_auc'
-    cv=10
-    max_evals=30
+def make_clf(class_weight, random_seed, **kwargs):
+    clf = RandomForestClassifier(
+            n_estimators=int(kwargs['clf.n_estimators']),
+            criterion=kwargs['clf.criterion'],
+            max_depth=kwargs['clf.max_depth'],
+            min_samples_split=kwargs['clf.min_samples_split'],
+            min_samples_leaf=kwargs['clf.min_samples_leaf'],
+            #min_weight_fraction_leaf=kwargs['clf.min_weight_fraction_leaf'],
+            max_features=kwargs['clf.max_features'],
+            class_weight=class_weight,
+            random_state=random_seed,
+           )
+    return clf
 
-@ex.automain
-def run(random_seed,
-        n_estimators,
+def subtrial_run(
+        random_seed,
+        shuffle,
+        onehot,
         class_weight,
+        imp_cut,
         scoring,
         cv,
         max_evals,
+        feature_importances=None,
        ):
-    X, y, X_unk = fw_data.get_data()
-    def objective(args):
-        clf = RandomForestClassifier(
-                n_estimators=n_estimators,
-                criterion=args['clf.criterion'],
-                max_depth=args['clf.max_depth'],
-                min_samples_split=args['clf.min_samples_split'],
-                min_samples_leaf=args['clf.min_samples_leaf'],
-                #min_weight_fraction_leaf=args['clf.min_weight_fraction_leaf'],
-                max_features=args['clf.max_features'],
-                class_weight=class_weight,
-                random_state=random_seed,
-               )
-        scores = cross_val_score(clf, X, y, scoring=scoring, cv=cv)
-        losses = [1. - s for s in scores]  # Want to minimize negative score
-        return {'loss': np.mean(losses), 'std_loss': np.std(losses), 'status': STATUS_OK}
+    X, y, X_unk = fw_data.get_data(onehot=onehot, seed=random_seed)
+    if feature_importances is not None and imp_cut > 0.:
+        print('Cutting by feature importance %f' % imp_cut)
+        feature_mask = feature_importances <= imp_cut
+        X = X.loc[:, feature_mask]
+        X_unk = X_unk.loc[:, feature_mask]
+        print('  Number of features is now %d' % X.shape[1])
+    if shuffle:
+        X, y = sk_shuffle(X, y, random_state=random_seed)
+    def objective(hp_args):
+        pprint(hp_args)
+        clf = make_clf(class_weight, random_seed, **hp_args)
+        scores = cross_val_score(clf, X, y, groups=y, scoring=scoring, cv=cv)
+        mean_score, std_score = np.mean(scores), np.std(scores)
+        print('  Mean  %s  is   %.3f +- %.3f' % (scoring, mean_score, std_score))
+        # We want to minimize the negative score
+        return {'loss': -mean_score, 'std_loss': std_score, 'status': STATUS_OK, 'clf': clf}
     space = get_rfc_space()
-    best = fmin(objective, space, algo=tpe.suggest, max_evals=max_evals)
-    print(best)
-#    util.print_feature_importances(clf, X, y)
-#    print('Mean %s score over %d CV-trials is %f +- %f' % (scoring, cv, np.mean(scores), np.std(scores)))
+    T = Trials()
+    best = fmin(objective, space, algo=tpe.suggest, max_evals=max_evals, trials=T)
+    best = space_eval(space, best)
+    with open('results/%s' % scoring, 'a') as f:
+        f.write('%s = %f\n' % (scoring, -T.losses()[-1]))
+        f.write('  %s' % X.shape[1])
+        f.write('  %s, onehot=%d, imp_cut=%f, cv=%d, rfc_evals=%d, seed=%d, shuffled=%d\n' % (class_weight, int(onehot), imp_cut, cv, max_evals, random_seed, int(shuffle)))
+        pprint(best)
+    return best
+
+
+def run(
+        random_seed,
+        shuffle,
+        onehot,
+        class_weight,
+        imp_cut,
+        scoring,
+        scoring_buffer,
+        cv,
+        max_evals,
+        n_rec,
+       ):
+    print('Should do %d subruns' % n_rec)
+    X, y, X_unk = fw_data.get_data(onehot, seed=random_seed)
+    score, last_score = None, None
+    best = {}
+    clf = None
+    feature_importances = None
+    while n_rec > 0:
+        print('Recursive (meta-feature analysis) step: %d' % n_rec)
+        if (last_score is None or score > last_score - scoring_buffer) and n_rec > 0:
+            last_score = score
+            best = subtrial_run(
+                random_seed,
+                shuffle,
+                onehot,
+                class_weight,
+                imp_cut,
+                scoring,
+                cv,
+                max_evals,
+                feature_importances=feature_importances,
+            )
+            clf = make_clf(class_weight, random_seed, **best).fit(X, y)
+            score = cross_val_score(clf, X, y, scoring=scoring)
+            score = np.mean(score)
+            print('Got  %s = %.4f after %.4f' % (scoring, score, (last_score or -1.)))
+            feature_importances = clf.feature_importances_
+        else:
+            print('Stop')
+            break
+        n_rec -= 1
+
+if __name__ == '__main__':
+    run(**cfg)
 
--- hyperspaces/rfc.py
+++ hyperspaces/rfc.py
@@ -4,11 +4,11 @@ from hyperopt import hp
 
 def get_space():
     space = {
-        #'clf.n_estimators': hp.qloguniform('n_estimators', 0., 4., 1),
+        'clf.n_estimators': hp.qloguniform('n_estimators', 2., 4., 1),
         'clf.criterion': hp.choice('criterion', ['gini', 'entropy']),
-        'clf.max_depth': hp.quniform('max_depth', 1, 8, 1),
-        'clf.min_samples_split': hp.loguniform('min_samples_split', -3, log(.5)),
-        'clf.min_samples_leaf': hp.loguniform('min_samples_leaf', -3, log(.5)),
+        'clf.max_depth': hp.quniform('max_depth', 8, 18, 1),
+        'clf.min_samples_split': hp.loguniform('min_samples_split', -4, -1),
+        'clf.min_samples_leaf': hp.loguniform('min_samples_leaf', -4, -1),
         #'clf.min_weight_fraction_leaf': hp.uniform('min_weight_fraction_leaf', 0., .1),
         'clf.max_features': hp.choice('max_features', ['auto', 'sqrt', 'log2']),
     }
